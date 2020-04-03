70354df4cd829263baaa8e36e9001041b455b078
brahmadaityo <53709038+brahmadaityo@users.noreply.github.com>
2020-04-03 23:37:57 +0530

    template for competitive programming

---
+++ template.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define fo(i,j,k) for(auto i=j;i<k;i++)
+#define forr(i,j,k) for(auto i=j;i>k;i--)
+#define max(a,b) a>b?a:b
+#define min(a,b) a<b?a:b
+#define vectli vector<long long int>
+#define vecti vector<int>
+#define vect2di vector<vector<i>>
+#define vects vector<string>
+#define PB push_back
+#define MP make_pair 
+#define F first
+#define S second
+void solve()
+{
+	
+}
+int main()
+{
+	//ll j,k,q,r,s;
+	ll m,n,p,t;
+	//vectli v; /*vector for input*/ 
+	scanf("%lld",&t); /*test case no input*/
+	while(t--)
+	{
+//	scanf("%lld",&n); /*single integer input*/
+//	scanf("%lld %lld",&m,&n); /*two integer input*/
+//	scanf("%lld %lld %lld",&m,&n,&p); /*three integer input*/
+//	fo(i,0,n) /*vector scanning*/ 
+//	{
+//	scanf("%lld",&p);
+//	v.PB(p);
+//	}
+
+	}
+	return 0;
+}
