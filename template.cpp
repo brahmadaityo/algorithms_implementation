73cdc923216e6186216db9fbc83aa1ed33db3ed9
Siddhartha Ghosal <53709038+brahmadaityo@users.noreply.github.com>
2020-04-08 12:01:52 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,27 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long int ll;
+using ll = long long int ;
 #define fo(i,j,k) for(auto i=j;i<k;i++)
 #define forr(i,j,k) for(auto i=j;i>k;i--)
 #define max(a,b) a>b?a:b
 #define min(a,b) a<b?a:b
-#define vectli vector<long long int>
-#define vecti vector<int>
-#define vect2di vector<vector<i>>
-#define vects vector<string>
+#define vec(i) vector<i>
+#define vec2d(i) vector<vector<i>>
+#define hashtab(i,j) unordered_map< i, vector<j> >
+#define hash(i,j) unordered_map<i,j>
+#define set(i) set<i>
 #define PB push_back
 #define MP make_pair 
 #define F first
 #define S second
+#define FASTIO std::ios_base::sync_with_stdio(0);std::cin.tie(NULL)
+#define scans(str) scanf("%s",&str)
+#define scan2s(str1,str2) scanf("%s %s",&str1,&str2)
+
 void solve()
 {
 	
 }
 int main()
 {
-	//ll j,k,q,r,s;
-	ll m,n,p,t;
-	//vectli v; /*vector for input*/ 
+	FASTIO;
+	ll j,k,q,r,s,m,n,p,t;
+	//vec(ll) v; /*vector for input*/ 
 	scanf("%lld",&t); /*test case no input*/
 	while(t--)
 	{
@@ -30,8 +35,7 @@ int main()
 //	scanf("%lld %lld %lld",&m,&n,&p); /*three integer input*/
 //	fo(i,0,n) /*vector scanning*/ 
 //	{
-//	scanf("%lld",&p);
-//	v.PB(p);
+	
 //	}
 
 	}
